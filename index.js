34c91da67bea7d11a1ba239a4777290bbe95f3aa
jstirpe <jennstirpe@yahoo.com>
2022-03-27 18:15:51 -0400

    Added mobile and desktop styles

---
+++ index.js
@@ -0,0 +1,13 @@
+const questions = document.querySelectorAll(".question");
+const answers = document.querySelectorAll(".answer");
+const arrows = document.querySelectorAll(".arrow");
+
+
+// console.log(questions.childNodes[1])
+// console.log(answers)
+// console.log(arrows)
+
+
+// question.innerText = question text
+// .childNodes[2] = img.arrow
+// question[i] answer[i]
