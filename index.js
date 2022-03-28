b26a17d2a250040570cb13b9912b0dc54210e135
jstirpe <jennstirpe@yahoo.com>
2022-03-28 18:28:48 -0400

    Added open/close functionality to show and hide answers when clicked

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+
 const questions = document.querySelectorAll(".question");
-const answers = document.querySelectorAll(".answer");
-const arrows = document.querySelectorAll(".arrow");
 
+for(let question of questions) {
+    question.addEventListener("click", () => {
+        let arrow = question.firstChild.nextSibling;
+        let answer = question.nextElementSibling;
 
-// console.log(questions.childNodes[1])
-// console.log(answers)
-// console.log(arrows)
+        // check if question clicked is already opened; if so, close it
+        if(question.classList.contains("active")) {
+            question.classList.remove("active");
+            arrow.classList.remove("open");
+            answer.classList.add("hidden");
+        } else {
+            // check if another question is open; if so, close it
+            questions.forEach(question => {
+                let arrow = question.firstChild.nextSibling;
+                let answer = question.nextElementSibling;
+                
+                if(question.classList.contains("active")) {
+                    question.classList.remove("active");
+                    arrow.classList.remove("open");
+                    answer.classList.add("hidden");  
+                }
+            })
 
+            // open the question that was clicked
+            question.classList.add("active");
+            arrow.classList.add("open");
+            answer.classList.remove("hidden");
+        }
+    })
+}
 
-// question.innerText = question text
-// .childNodes[2] = img.arrow
-// question[i] answer[i]
